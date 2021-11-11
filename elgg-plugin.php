74481fa479ef2ec217f3363c1681e40dc0a05f58
Hermand PESSEK <pessek@bitbucket.org>
2021-11-11 18:23:36 -0500

    Release 4.1

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -2,7 +2,7 @@
 
 return [
         'plugin' => [
-                'version' => '4.0',
+                'version' => '4.1',
 		'name' => 'Pessek Interactions',
                 'dependencies' => [
                         'pessek_stash' => [],
